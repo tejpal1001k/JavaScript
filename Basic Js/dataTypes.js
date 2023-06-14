d3659c8659e5694a41382fa104476bb6ea6b82ba
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-06-14 18:27:55 +0530

    All type of datatypes like number, boolean, string, undefined, null or object

---
+++ Basic Js/dataTypes.js
@@ -0,0 +1,22 @@
+// number
+var x  = 10;
+console.log(typeof(x));
+
+x = 2.5;
+console.log(typeof(x));
+
+// boolean
+var x = true;
+console.log(typeof(x));
+
+// Strings
+x = "Hello I am a string";
+console.log(typeof(x));
+
+// undefined
+var a;
+console.log(typeof(a));
+
+// null is a object
+var a = null;
+console.log(typeof(a));
