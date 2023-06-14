c8d6f8cf380675c8bb911493022d2a89264247d6
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-06-14 17:57:46 +0530

    Javascript arithmatic operations

---
+++ Basic Js/arithmaticOperations.js
@@ -0,0 +1,23 @@
+// Addition
+
+var x =10;
+var y =21;
+var c = x+y;
+console.log("addition = "+ c);
+
+//Subtraction
+
+c = x-y;
+console.log("Subtraction = "+c);
+
+//multiplication
+
+c = x*y;
+console.log("multiplication = " + c);
+
+//division
+console.log("division = "+ c);
+
+// Modulas 
+c = y%x;
+console.log("mod = "+ c);
--- Basic Js/helloJs.js
+++ Basic Js/helloJs.js
@@ -1,4 +1,4 @@
 console.log("Hello Java Script");
 let x=1;
 let y=2;
-console.log(x+y)
+console.log(x+y);
