ce661caa5d72b0a153eef0bc394fbc19f2194864
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-06-14 19:50:19 +0530

    All basic loops like for,do-while, while in this program

---
+++ Basic Js/allLoops.js
@@ -0,0 +1,21 @@
+var count;
+for(count = 0; count <= 10; count++){
+    console.log("print: " + count);
+}
+
+
+// While loop
+var x = 12;
+while(x >= 0){
+    console.log("print "+ x--);
+}
+
+// do while loop
+var x = 10;
+do{
+    console.log("print "+ x--);
+}
+while(x>=0)
+
+
+// for in loops we will dicuss in later
