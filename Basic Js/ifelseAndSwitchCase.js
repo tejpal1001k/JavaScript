b2322bac34dcd7689df5b18936c826e7781bcee5
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-06-14 19:16:08 +0530

    Here in this code all if-else and swtich case discussed

---
+++ Basic Js/ifelseAndSwitchCase.js
@@ -0,0 +1,41 @@
+
+
+var a = 10;
+var b = 20; 
+// var b = false; 
+if(a >= b){
+    console.log("a is greather then b");
+}else{
+    console.log("b is greather then a");  
+}
+
+// all the days of a week 1 to 7
+
+let day = 3;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Not availabe in a week of this day ");
+        break;
+        
+}
