dab1db215d0f2b3eaf9ffabd931ad90fa051c15d
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-06-14 18:18:01 +0530

    variables

---
+++ Basic Js/variables.js
@@ -0,0 +1,9 @@
+
+
+var a = 10;
+var a = 12;//no error
+
+let b = 10;
+// let b = 12; will show error we can't redeclare in let
+const c = 10;
+// c = 20; will show error can't update the value of constant
