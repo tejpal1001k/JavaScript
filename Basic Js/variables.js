6496784a7f9395a7d74d1b710ff3a31c158f92a6
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-06-14 18:22:49 +0530

    All tye of variables like let, var, const, local,global

--- Basic Js/variables.js
+++ Basic Js/variables.js
@@ -7,3 +7,16 @@ let b = 10;
 // let b = 12; will show error we can't redeclare in let
 const c = 10;
 // c = 20; will show error can't update the value of constant
+
+
+// Scoe vairables
+
+
+
+// local variables
+function add(){
+    var x = 10;    //here x and b are local variables and above the variables are the global variables 
+    let b = 20;
+    console.log(x+b);
+}
+add();
